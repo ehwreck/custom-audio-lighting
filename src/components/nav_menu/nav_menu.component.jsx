0846b9a6cf521127ebda230daa4df06016ccb4e2
Erick Trevino <ericktrevino@Ericks-MBP.lan>
2024-01-19 21:39:16 -0600

    Add responsive styling to navigation menu.

--- src/components/nav_menu/nav_menu.component.jsx
+++ src/components/nav_menu/nav_menu.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './nav_menu.styles.css';
 import LinkDropdown from '../link_dropdown/link_dropdown.component';
 import { Link } from 'react-router-dom';
@@ -6,26 +6,41 @@ import routes from '../../routes/routes';
 import capitalize from '../../helpers/capitalize.js';
 import InteractiveButton from '../interactive_button/interactive_button.component.jsx';
 import InteractiveLink from '../interactive_link/interactive_link.component.jsx';
+import useWindowDimensions from '../../hooks/useWindowDimensions.js';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const NavMenu = (props) => {
   const { showLogo, logoURL, logoAltText, companyName, companyMotto } = props;
+  const { width } = useWindowDimensions();
+
+  const [showCollapsibleMenu, setShowCollapsibleMenu] = useState(width < 1200);
+  const [collapsibleMenuOpen, setCollapsibleMenuOpen] = useState(false);
+
+  useEffect(()=>{
+    setShowCollapsibleMenu(width < 1200);
+  }, [width]);
 
   return (
-    <div id="main_header">
-      <div className="nav_menu_container">
-        <div id="left_nav_section">
+    <div id="main-header">
+      <div id="nav-menu-container">
+        <div id="left-nav-section">
           {showLogo && (
             <img id="nav_menu_logo" src={logoURL} alt={logoAltText} />
           )}
-          <div id="company_label">
-            <Link id="nav_menu_name" to="/">
+          <div id="company-label">
+            <Link to="/">
               {companyName}
             </Link>
-            <span id="nav_menu_motto">{companyMotto}</span>
+            <span>{companyMotto}</span>
           </div>
         </div>
-        <div id="right_nav_section">
-          <div id="options">
+        <div id='right-nav-section'>
+          <div id='collapsible-icon' className={width < 1200 ? '' : ' hidden'}>
+            <MenuIcon className={showCollapsibleMenu && !collapsibleMenuOpen ? '' : 'hidden'} onClick={()=>{setCollapsibleMenuOpen(true)}}/>
+            <CloseIcon className={showCollapsibleMenu && collapsibleMenuOpen ? '' : 'hidden'} onClick={()=>{setCollapsibleMenuOpen(false)}}/>
+          </div>
+          <div id="options" className={!showCollapsibleMenu ? '' : (collapsibleMenuOpen ? '' : 'hidden')}>
             {routes.slice(1).map((route) => {
               if (route.type === 'dropdown-link') {
                 return (
